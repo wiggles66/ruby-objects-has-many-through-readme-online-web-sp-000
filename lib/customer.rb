20f6eb4cdb309fd8ad3e65d9f09b5fec4d233381
Michael Waide <mvwaide@gmail.com>
2020-07-21 17:03:45 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -27,6 +27,19 @@ def waiters
   meals.map do |meal|
     meal.waiter
   end
+
+  terrance = Customer.new("Terrance", 27)
+jason = Waiter.new("Jason", 4)
+andrew = Waiter.new("Andrew", 7)
+yomi = Waiter.new("Yomi", 10)
+
+terrance.new_meal(jason, 50, 6)
+terrance.new_meal(andrew, 60, 8)
+terrance.new_meal(yomi, 30, 4)
+
+terrance.waiters #=> [#<Waiter:0x00007fa23f18f860 @name="Jason", @yrs_experience=34>, #<Waiter:0x00007fa23f196818 @name="Andrew", @yrs_experience=27>, #<Waiter:0x00007fa23f19dd20 @name="Yomi", @yrs_experience=20>]
+terrance.waiters.length #=> 3
+
 end
 
 alex = Customer.new("Alex", 30)
