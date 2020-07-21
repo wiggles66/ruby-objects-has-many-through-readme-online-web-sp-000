06ec7cf9e9ec6915d928b5cf91392b8cd7a194d0
Michael Waide <mvwaide@gmail.com>
2020-07-21 17:03:16 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -23,5 +23,17 @@ class Customer
   end
 end
 
+alex = Customer.new("Alex", 30)
+rachel = Customer.new("Rachel", 27)
+dan = Waiter.new("Dan", 3)
+
+rachel.new_meal(dan, 50, 10)
+alex.new_meal(dan, 30, 5)
+
+rachel.meals #=> [#<Meal:0x00007fa23f1575a0 @waiter=#<Waiter:0x00007fa23f14fbe8 @name="Dan", @yrs_experience=22>, @customer=#<Customer:0x00007fa240987468 @name="Rachel", @age=27>, @total=50, @tip=10>]
+rachel.meals.length #=> 1
+
+Meal.all.length
+
 
 end
